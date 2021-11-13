b41e93fbfbc450878b5d8f440959f630d26697e8
Vitorrecoaro <vitordearec@gmail.com>
2021-11-13 12:39:00 -0300

    Arrumando erro da tela de mural.

--- lib/pages/mural_page.dart
+++ lib/pages/mural_page.dart
@@ -45,7 +45,7 @@ class _MuralProjetoState extends State<MuralProjeto> {
       value: _toDo[index]["finished"],
       onChanged: (c) {
         setState(
-              () {
+          () {
             _toDo[index]["finished"] = c;
           },
         );
@@ -78,93 +78,95 @@ class _MuralProjetoState extends State<MuralProjeto> {
       appBarTitle: Text(AppLocalizations.of(context)!.mural_page_title),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-        child: ListView(
-          children: [
-            Text(
-              projectName,
-              style: const TextStyle(
-                  fontFamily: "Roboto",
-                  fontWeight: FontWeight.w700,
-                  fontSize: 30),
-              textAlign: TextAlign.center,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: Column(
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Flexible(
-                        flex: 2,
-                        child: BallonContainer(
-                            onTap: () {
-                              setState(() {
-                                readMoreObj = !readMoreObj;
-                              });
-                            },
-                            backGroundColor: Color(0xFFFBAB48),
-                            borderRadius: BorderRadius.only(
-                                topRight: Radius.circular(10),
-                                topLeft: Radius.circular(10),
-                                bottomLeft: Radius.circular(10)),
-                            title: "Objetivos",
-                            child: Text(goal,
-                                maxLines: readMoreObj ? null : 3,
-                                overflow: readMoreObj
-                                    ? TextOverflow.visible
-                                    : TextOverflow.ellipsis,
-                                style: const TextStyle(
-                                    fontSize: 18, fontFamily: "Comfortaa")),
-                            readMore: readMoreObj,
-                            cndLeiaMais: goal.length > 50),
-                      ),
-                      Flexible(
-                        flex: 2,
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 15),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text(
+                projectName,
+                style: const TextStyle(
+                    fontFamily: "Roboto",
+                    fontWeight: FontWeight.w700,
+                    fontSize: 30),
+                textAlign: TextAlign.center,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Flexible(
+                          flex: 2,
                           child: BallonContainer(
-                            onTap: () {
-                              setState(() {
-                                readMoreMem = !readMoreMem;
-                              });
-                            },
-                            backGroundColor: Color(0xFFC48BDC),
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10),
-                              topRight: Radius.circular(10),
-                              bottomRight: Radius.circular(10),
+                              onTap: () {
+                                setState(() {
+                                  readMoreObj = !readMoreObj;
+                                });
+                              },
+                              backGroundColor: Color(0xFFFBAB48),
+                              borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(10),
+                                  topLeft: Radius.circular(10),
+                                  bottomLeft: Radius.circular(10)),
+                              title: "Objetivos",
+                              child: Text(goal,
+                                  maxLines: readMoreObj ? null : 3,
+                                  overflow: readMoreObj
+                                      ? TextOverflow.visible
+                                      : TextOverflow.ellipsis,
+                                  style: const TextStyle(
+                                      fontSize: 18, fontFamily: "Comfortaa")),
+                              readMore: readMoreObj,
+                              cndLeiaMais: goal.length > 65),
+                        ),
+                        Flexible(
+                          flex: 2,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 15),
+                            child: BallonContainer(
+                              onTap: () {
+                                setState(() {
+                                  readMoreMem = !readMoreMem;
+                                });
+                              },
+                              backGroundColor: Color(0xFFC48BDC),
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(10),
+                                topRight: Radius.circular(10),
+                                bottomRight: Radius.circular(10),
+                              ),
+                              cndLeiaMais: members.length > 4,
+                              readMore: readMoreMem,
+                              title: 'Membros',
+                              child: ListView.builder(
+                                  shrinkWrap: true,
+                                  itemCount: readMoreMem
+                                      ? members.length
+                                      : (members.length < 3
+                                          ? members.length
+                                          : 3),
+                                  itemBuilder: lMemberBuilder),
                             ),
-                            cndLeiaMais: members.length > 4,
-                            readMore: readMoreMem,
-                            title: 'Membros',
-                            child: ListView.builder(
-                                shrinkWrap: true,
-                                itemCount: readMoreMem
-                                    ? members.length
-                                    : (members.length < 3 ? members.length : 3),
-                                itemBuilder: lMemberBuilder),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: _paddingColumn),
-                    child: BallonContainer(
-                      backGroundColor: Color(0xFFF0766B),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
-                      ),
-                      cndLeiaMais: meets.length > 4,
-                      onTap: () {
-                        setState(() {
-                          readMoreMeet = !readMoreMeet;
-                        });
-                      },
-                      readMore: readMoreMeet,
-                      title: 'Próximas Reuniões',
-                      child: Expanded(
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: _paddingColumn),
+                      child: BallonContainer(
+                        backGroundColor: Color(0xFFF0766B),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                        cndLeiaMais: meets.length > 4,
+                        onTap: () {
+                          setState(() {
+                            readMoreMeet = !readMoreMeet;
+                          });
+                        },
+                        readMore: readMoreMeet,
+                        title: 'Próximas Reuniões',
                         child: ListView.builder(
                           shrinkWrap: true,
                           itemCount: readMoreMeet
@@ -174,35 +176,35 @@ class _MuralProjetoState extends State<MuralProjeto> {
                         ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: _paddingColumn),
-                    child: BallonContainer(
-                      backGroundColor: Color(0xFF61CFD7),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
+                    Padding(
+                      padding: const EdgeInsets.only(top: _paddingColumn),
+                      child: BallonContainer(
+                        backGroundColor: Color(0xFF61CFD7),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                        cndLeiaMais: _toDo.length > 4,
+                        onTap: () {
+                          setState(() {
+                            readMoreTsk = !readMoreTsk;
+                          });
+                        },
+                        readMore: readMoreTsk,
+                        title: 'Tarefas',
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          itemBuilder: listBuilder,
+                          itemCount: readMoreTsk
+                              ? _toDo.length
+                              : (_toDo.length < 3 ? _toDo.length : 3),
+                        ),
                       ),
-                      cndLeiaMais: _toDo.length > 4,
-                      onTap: () {
-                        setState(() {
-                          readMoreTsk = !readMoreTsk;
-                        });
-                      },
-                      readMore: readMoreTsk,
-                      title: 'Tarefas',
-                      child: ListView.builder(
-                        shrinkWrap: true,
-                        itemBuilder: listBuilder,
-                        itemCount: readMoreTsk
-                            ? _toDo.length
-                            : (_toDo.length < 3 ? _toDo.length : 3),
-                      ),
-                    ),
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
