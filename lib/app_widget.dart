8981b9988ac6a16deadff065adaa15db49feffc6
Vitorrecoaro <vitordearec@gmail.com>
2021-11-18 11:59:21 -0300

    Fazendo a primeira parte do front da tela.

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -8,6 +8,7 @@ import 'package:pet_mobile/pages/help_page.dart';
 import 'package:pet_mobile/pages/home_page.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:pet_mobile/pages/mural_page.dart';
+import 'package:pet_mobile/pages/adicionar_projeto_page.dart';
 
 import 'pages/LoginPage.dart';
 import 'config/themes/theme.dart';
@@ -20,9 +21,10 @@ import 'config/themes/theme.dart';
 class AppWidget extends StatelessWidget {
   AppWidget({Key? key}) : super(key: key);
 
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      onGenerateTitle: (BuildContext context) => AppLocalizations.of(context)!.title,
+      onGenerateTitle: (BuildContext context) =>
+          AppLocalizations.of(context)!.title,
       theme: AppTheme.theme,
       // i18n
       localizationsDelegates: AppLocalizations.localizationsDelegates,
@@ -38,6 +40,7 @@ class AppWidget extends StatelessWidget {
         '/meet': (context) => MeetPage(),
         '/login': (context) => LoginPage(),
         '/mural': (context) => MuralProjeto(),
+        '/adicionar': (context) => AdicionarProjeto(),
       },
     );
   }
