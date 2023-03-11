a623d8955aebc1ac863be9a7952fa99afd6326ea
brunol-pereira <brunoleandropereira@hotmail.com>
2023-03-11 15:42:54 -0300

    Alterando inputs e add input data

--- lib/pages/meet_page.dart
+++ lib/pages/meet_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pet_mobile/widgets/side_menu_scaffold_with_profile_header.dart';
+import 'package:intl/intl.dart';
 
 class MeetPage extends StatefulWidget {
   @override
@@ -7,52 +8,175 @@ class MeetPage extends StatefulWidget {
 }
 
 class _MeetPageState extends State<MeetPage> {
+  TextEditingController dateController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    dateController.text = "";
+  }
+
   @override
   Widget build(BuildContext context) {
     return SideMenuScaffoldWithProfileHeader(
       appBarTitle: Text('Meet Page'),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            CustomTextField(hint: 'Assunto...', icon: Icons.subject),
-            SizedBox(height: 15),
-            CustomTextField(hint: 'Descrição...', icon: Icons.description),
-            SizedBox(height: 15),
-            CustomTextField(
-                hint: 'Data...', icon: Icons.calendar_today_outlined),
-            SizedBox(height: 15),
-            ElevatedButton(
-              onPressed: () {
-                createAlertDialog(context);
-              },
-              child: Row(
-                children: [
-                  Icon(Icons.access_time, color: Colors.white70),
-                  Text("  Horário"),
-                ],
-              ),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.all(18),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                textStyle: TextStyle(color: Colors.white),
-              ),
+      body: Container(
+        padding: EdgeInsets.only(
+          top: 60,
+          left: 40,
+          right: 40,
+        ),
+        color: Colors.white,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              height: 20,
             ),
-            // CustomTextField(hint: 'Horário...', icon: Icons.access_time),
-            SizedBox(height: 15),
-            CustomTextField(
-                hint: 'Membros Participantes...', icon: Icons.person),
-            SizedBox(height: 15),
-            CustomTextField(hint: 'Link Reunião...', icon: Icons.link),
-            SizedBox(height: 15),
-            CustomTextField(hint: 'Anexar...', icon: Icons.attach_file),
-          ]),
+            Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Material(
+                    color: Colors.transparent,
+                    child: TextFormField(
+                      style: const TextStyle(
+                          fontFamily: "Montserrat", fontSize: 15),
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          hintText: "Assunto",
+                          prefixIcon: Icon(Icons.subject)),
+                    ))),
+            Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Material(
+                    color: Colors.transparent,
+                    child: TextFormField(
+                      style: const TextStyle(
+                          fontFamily: "Montserrat", fontSize: 15),
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          hintText: "Descrição",
+                          prefixIcon: Icon(Icons.description)),
+                    ))),
+            Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Material(
+                    color: Colors.transparent,
+                    child: TextFormField(
+                      style: const TextStyle(
+                          fontFamily: "Montserrat", fontSize: 15),
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          hintText: "Link Reunião",
+                          prefixIcon: Icon(Icons.link)),
+                    ))),
+            Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Material(
+                    color: Colors.transparent,
+                    child: TextFormField(
+                      controller: dateController,
+                      style: const TextStyle(
+                          fontFamily: "Montserrat", fontSize: 15),
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          hintText: "Data",
+                          prefixIcon: Icon(Icons.calendar_today)),
+                      readOnly: true,
+                      onTap: () async {
+                        DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2101),
+                        );
+                        if (pickedDate != null) {
+                          String formattedDate =
+                              DateFormat("dd/MM/yyyy").format(pickedDate);
+
+                          setState(() {
+                            dateController.text = formattedDate.toString();
+                          });
+                        } else {
+                          print("Not selected");
+                        }
+                      },
+                    ))),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
+              child: Container(
+                  height: 50.0,
+                  child: ElevatedButton(
+                    child: Text('Pronto'),
+                    style: TextButton.styleFrom(
+                      shape: const BeveledRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5))),
+                    ),
+                    onPressed: () {},
+                  )),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+//Antigos inputs
+
+// class _MeetPageState extends State<MeetPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return SideMenuScaffold(
+//       appBarTitle: Text('Meet Page'),
+//       body: SingleChildScrollView(
+//         child: Padding(
+//           padding: const EdgeInsets.all(20.0),
+//           child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+//             CustomTextField(hint: 'Assunto...', icon: Icons.subject),
+//             SizedBox(height: 15),
+//             CustomTextField(hint: 'Descrição...', icon: Icons.description),
+//             SizedBox(height: 15),
+//             CustomTextField(
+//                 hint: 'Data...', icon: Icons.calendar_today_outlined),
+//             SizedBox(height: 15),
+//             ElevatedButton(
+//               onPressed: () {
+//                 createAlertDialog(context);
+//               },
+//               child: Row(
+//                 children: [
+//                   Icon(Icons.access_time, color: Colors.white70),
+//                   Text("  Horário"),
+//                 ],
+//               ),
+//               style: ElevatedButton.styleFrom(
+//                 padding: EdgeInsets.all(18),
+//                 shape: RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.circular(20)),
+//                 textStyle: TextStyle(color: Colors.white),
+//               ),
+//             ),
+//             // CustomTextField(hint: 'Horário...', icon: Icons.access_time),
+//             SizedBox(height: 15),
+//             CustomTextField(
+//                 hint: 'Membros Participantes...', icon: Icons.person),
+//             SizedBox(height: 15),
+//             CustomTextField(hint: 'Link Reunião...', icon: Icons.link),
+//             SizedBox(height: 15),
+//             CustomTextField(hint: 'Anexar...', icon: Icons.attach_file),
+//           ]),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 class CustomTextField extends StatelessWidget {
   final String hint;
