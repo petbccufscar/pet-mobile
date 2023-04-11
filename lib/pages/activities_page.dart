62db0bc249da0f2cf71d72915311fa65302da7f4
caio <uscaio@estudante.ufscar.br>
2023-04-11 16:03:58 -0300

    fix: _ver mais_ e _ marcar atividade_ transformados em botão

--- lib/pages/activities_page.dart
+++ lib/pages/activities_page.dart
@@ -69,179 +69,31 @@ class Atividade extends StatelessWidget {
                 textStyle: TextStyle(color: Colors.white),
               ),
             ),
-            ElevatedButton(
-              onPressed: (){
-                //acessa a atividade
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Atividade Modelo 4"),
-                ],
-              ),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.all(13),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                textStyle: TextStyle(color: Colors.white),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: (){
-                //acessa a atividade
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Atividade Modelo 5"),
-                ],
-              ),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.all(13),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                textStyle: TextStyle(color: Colors.white),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: (){
-                //acessa a atividade
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Atividade Modelo 6"),
-                ],
-              ),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.all(13),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                textStyle: TextStyle(color: Colors.white),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: (){
-                //acessa a atividade
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Atividade Modelo 7"),
-                ],
-              ),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.all(13),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                textStyle: TextStyle(color: Colors.white),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: (){
-                //acessa a atividade
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Atividade Modelo 8"),
-                ],
-              ),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.all(13),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                textStyle: TextStyle(color: Colors.white),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: (){
-                //acessa a atividade
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Atividade Modelo 9"),
-                ],
-              ),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.all(13),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                textStyle: TextStyle(color: Colors.white),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: (){
-                //acessa a atividade
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Atividade Modelo 10"),
-                ],
-              ),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.all(13),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                textStyle: TextStyle(color: Colors.white),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: (){
-                //acessa a atividade
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Atividade Modelo 11"),
-                ],
-              ),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.all(13),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                textStyle: TextStyle(color: Colors.white),
-              ),
-            ),
             //Spacer(),
-            Align(
-              alignment: Alignment.bottomCenter,
-              /*
-                child: ElevatedButton(
-                  onPressed: () {
-                    //ver mais
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white, // Background color
-                  ),
-                  child: Text('ver mais',
-                style: TextStyle(color: Colors.blue),
-                  ),
-                )
-                  */
-              child: Text('ver mais',
-                style: TextStyle(color: Colors.blue),
+            Positioned(
+              bottom: 0,
+              child: TextButton(
+                  onPressed: () {},
+                  child: Text.rich(
+                    TextSpan(
+                      text: 'ver mais',
+                      style: TextStyle(color: Colors.blue),
+                    )
+                  )
+                ),
+              ),
+            Positioned(
+              bottom: 0,
+              child: TextButton(
+                  onPressed: () {},
+                  child: Text.rich(
+                      TextSpan(
+                        text: 'marcar atividade',
+                        style: TextStyle(color: Colors.blue),
+                      )
+                  )
               ),
             ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Text('marcar atividade',
-                style: TextStyle(color: Colors.blue),
-              ),
-            )
-            /*
-              PlaceHolders Apenas para visualização de como ficará
-              CustomTextField(hint: 'Atividade PlaceHolder01'),
-              SizedBox(height: 15),
-              CustomTextField(hint: 'Atividade PlaceHolder03'),
-              SizedBox(height: 15),
-              CustomTextField(hint: 'Atividade PlaceHolder04'),
-              SizedBox(height: 15),
-              CustomTextField(hint: 'Atividade PlaceHolder05'),
-              SizedBox(height: 15),
-               */
           ]),
         ),
       ),
