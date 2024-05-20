ceaeebf526020832efade67e093bdc9b772be1be
JoaoOLM <joao.moraes@estudante.ufscar.br>
2024-05-19 23:15:03 -0300

    Alterações para integração

---
+++ lib/provider/api_service.dart
@@ -0,0 +1,20 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ApiService {
+  final String UrlAppend;
+
+  ApiService({required this.UrlAppend});
+
+  Future<List<dynamic>> fetchData() async {
+                                              /* Subsitua por http://(seu indereço de ip):8000*/
+    final response = await http.get(Uri.parse('http://192.168.0.53:8000/api/v2/$UrlAppend'));
+
+    if (response.statusCode == 200) {
+      List data = json.decode(response.body);
+      return data;
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+}
