0c5c4fd48641a2a1ca5c37758803940bff7e0372
KY-U <uscaio@estudante.ufscar.br>
2024-05-23 20:55:35 -0300

    feat: lista de projetos integrada

--- lib/pages/descricao_projeto_page.dart
+++ lib/pages/descricao_projeto_page.dart
@@ -135,3 +135,170 @@ class _DescricaoProjetoState extends State<DescricaoProjeto> {
     );
   }
 }
+/*
+import 'package:flutter/material.dart';
+import 'package:pet_mobile/provider/api_service.dart'; 
+
+class DescricaoProjeto extends StatefulWidget {
+  const DescricaoProjeto({Key? key}) : super(key: key);
+
+  @override
+  State<DescricaoProjeto> createState() => _DescricaoProjetoState();
+}
+
+class _DescricaoProjetoState extends State<DescricaoProjeto> {
+  late Future<List<dynamic>> futureProjects;
+  final ApiService apiService = ApiService(UrlAppend: 'projetos/');
+  /* 
+  final List<String> _membros = [
+    "Fulano",
+    "Ciclano",
+    "Beltrano"
+  ]; // adicionando membros
+  */
+
+  @override
+  void initState() {
+    super.initState();
+    futureProjects = apiService.fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double _screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: const Text("Descrição do projeto"),
+        centerTitle: true,
+      ),
+      body: FutureBuilder<List<dynamic>>(
+        future: futureProjects,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Erro: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('Nenhum projeto encontrado'));
+          } else {
+            List<dynamic> projects = snapshot.data!;
+            // Assumindo que você deseja exibir o primeiro projeto da lista
+            final project = projects[0];
+            return Stack(
+              alignment: Alignment.topCenter,
+              children: <Widget>[
+                Positioned(
+                  top: _screenHeight * .28,
+                  height: _screenHeight * .60,
+                  left: 0,
+                  right: 0,
+                  child: PageView(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  left: 15,
+                                  top: 10,
+                                  right: 15,
+                                ),
+                                child: Text(
+                                  project['title'], // Título do projeto do backend
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              _buildSectionTitle("Sobre o projeto"),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  left: 10,
+                                  top: 10,
+                                  right: 10,
+                                ),
+                                child: Text(
+                                  project['description'], // Descrição do projeto do backend
+                                  textAlign: TextAlign.justify,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ),
+                              _buildSectionTitle("Membros"),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  left: 10,
+                                  top: 10,
+                                ),
+                                child: Column(
+                                  children: (project['members'] as List<dynamic>)
+                                      .map((membro) => Text(
+                                            membro,
+                                            textAlign: TextAlign.justify,
+                                            style: const TextStyle(
+                                              fontSize: 18,
+                                            ),
+                                          ))
+                                      .toList(),
+                                ),
+                              ),
+                              _buildSectionTitle("Outras infos"),
+                              // Adicione outras seções conforme necessário
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: -10,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(15),
+                      bottomRight: Radius.circular(15),
+                    ),
+                    child: Image.network(
+                      project['image_url'], // URL da imagem do backend
+                      height: _screenHeight * .3,
+                      width: MediaQuery.of(context).size.width,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  Widget _buildSectionTitle(String title) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 10,
+        top: 10,
+      ),
+      child: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
+*/
--- lib/pages/projectsList.dart
+++ lib/pages/projectsList.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:pet_mobile/model/project.dart';
+import 'package:pet_mobile/model/project.dart'; //isso serve pra que mesmo?
 import 'package:pet_mobile/pages/descricao_projeto_page.dart';
 import 'package:pet_mobile/widgets/side_menu_scaffold_with_profile_header.dart';
+import 'package:pet_mobile/provider/api_service.dart';
 
 class ProjectsList extends StatefulWidget {
   const ProjectsList({Key? key}) : super(key: key);
@@ -11,21 +12,14 @@ class ProjectsList extends StatefulWidget {
 }
 
 class _ProjectsListState extends State<ProjectsList> {
-  List<Project> projectsList = [
-    Project(
-        title: 'Aplicativo Sororidade',
-        imageUrl: 'https://petbcc.ufscar.br/media/app-sororidade.jpg'),
-    Project(
-        title: 'Beecrowd', imageUrl: 'https://petbcc.ufscar.br/media/bee.png'),
-    Project(
-        title: 'Engenharia de Software Quântico',
-        imageUrl: 'https://petbcc.ufscar.br/media/quantico.png'),
-    Project(
-        title: 'Iuten', imageUrl: 'https://petbcc.ufscar.br/media/Logo.png'),
-    Project(
-        title: 'Manual de Referências em C/C++',
-        imageUrl: 'https://petbcc.ufscar.br/media/tutorial_linguagem_c.jpeg'),
-  ];
+  late Future<List<dynamic>> futureProjects;
+  final ApiService apiService = ApiService(UrlAppend: 'projetos/');
+
+  @override
+  void initState() {
+    super.initState();
+    futureProjects = apiService.fetchData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,87 +28,95 @@ class _ProjectsListState extends State<ProjectsList> {
       appBarElevation: 0,
       body: Container(
         decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
               Colors.blue,
-              //Color(0xFF1485BF),
               Color(0xFF61CFD7),
-            ])),
+            ],
+          ),
+        ),
         child: SafeArea(
-          child: Container(
-            margin: EdgeInsets.all(16),
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                childAspectRatio: MediaQuery.of(context).size.width /
-                    (MediaQuery.of(context).size.height / 1.4),
-                crossAxisCount: 2,
-                mainAxisSpacing: 10.0,
-                crossAxisSpacing: 10.0,
-              ),
-              itemCount: projectsList.length,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => DescricaoProjeto()),
-                    );
-                    /*Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ProjectsList(
-                          Project: projectsList[index],
-                          projectHeroTag: 'project-$index',
-                        ),
-                      ),
-                    );*/
-                  },
-                  child: Card(
-                    elevation: 6.0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
+          child: FutureBuilder<List<dynamic>>(
+            future: futureProjects,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Erro: ${snapshot.error}'));
+              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return Center(child: Text('Nenhum projeto encontrado'));
+              } else {
+                List<dynamic> projectsList = snapshot.data!;
+                return Container(
+                  margin: EdgeInsets.all(16),
+                  child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      childAspectRatio: MediaQuery.of(context).size.width /
+                          (MediaQuery.of(context).size.height / 1.4),
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 10.0,
+                      crossAxisSpacing: 10.0,
                     ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Expanded(
-                          flex: 2,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(12.0),
-                              topRight: Radius.circular(12.0),
+                    itemCount: projectsList.length,
+                    itemBuilder: (context, index) {
+                      var project = projectsList[index];
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DescricaoProjeto(),
                             ),
-                            child: Hero(
-                              tag: 'project-$index',
-                              child: Image.network(
-                                projectsList[index].getImageUrl,
-                                fit: BoxFit.cover,
+                          );
+                        },
+                        child: Card(
+                          elevation: 6.0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Expanded(
+                                flex: 2,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(12.0),
+                                    topRight: Radius.circular(12.0),
+                                  ),
+                                  child: Hero(
+                                    tag: 'project-$index',
+                                    child: Image.network(
+                                      project['imagem'], //ta certo isso?
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
                               ),
-                            ),
+                              Expanded(
+                                flex: 1,
+                                child: Center(
+                                  child: Text(
+                                    project['titulo'], // ta certo isso?
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                        Expanded(
-                          flex: 1,
-                          child: Center(
-                            child: Text(
-                              projectsList[index].getTitle, // max 60 caracteres
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+                      );
+                    },
                   ),
                 );
-              },
-            ),
+              }
+            },
           ),
         ),
       ),
