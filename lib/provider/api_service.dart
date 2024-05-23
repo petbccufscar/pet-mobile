7c0575f4844a0544d2a2ace833f414a669eb05f0
KY-U <uscaio@estudante.ufscar.br>
2024-05-23 17:25:45 -0300

    add: auto-dox

--- lib/provider/api_service.dart
+++ lib/provider/api_service.dart
@@ -8,7 +8,10 @@ class ApiService {
 
   Future<List<dynamic>> fetchData() async {
                                               /* Subsitua por http://(seu indereço de ip):8000*/
-    final response = await http.get(Uri.parse('http://192.168.0.53:8000/api/v2/$UrlAppend'));
+    //otavio
+    //final response = await http.get(Uri.parse('http://192.168.0.53:8000/api/v2/$UrlAppend'));
+    //kyu
+    final response = await http.get(Uri.parse('http://192.168.0.5:8000/api/v2/$UrlAppend'));
 
     if (response.statusCode == 200) {
       List data = json.decode(response.body);
