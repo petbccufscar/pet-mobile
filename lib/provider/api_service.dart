549b32cf66126b89f4aa6f4d4b396c56b9bac45a
KY-U <uscaio@estudante.ufscar.br>
2024-05-30 17:07:21 -0300

    ip de pedreira

--- lib/provider/api_service.dart
+++ lib/provider/api_service.dart
@@ -10,9 +10,13 @@ class ApiService {
                                               /* Subsitua por http://(seu indereço de ip):8000*/
     //otavio
     //final response = await http.get(Uri.parse('http://192.168.0.53:8000/api/v2/$UrlAppend'));
-    //kyu
-    final response = await http.get(Uri.parse('http://192.168.0.5:8000/api/v2/$UrlAppend'));
 
+    //kyu
+    //final response = await http.get(Uri.parse('http://192.168.0.5:8000/api/v2/$UrlAppend'));
+    
+    //kyu pedreira
+    
+    final response = await http.get(Uri.parse('http://192.168.15.108:8000/api/v2/$UrlAppend'));
     if (response.statusCode == 200) {
       List data = json.decode(response.body);
       return data;
